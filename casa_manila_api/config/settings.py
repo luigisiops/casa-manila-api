16c966aca3b258480eef7f39aff958163cdc1822
Luigi Siopongco <luigisiopongco@Luigis-MacBook-Pro.local>
2025-12-13 05:06:25 -0500

    added migrations

--- casa_manila_api/config/settings.py
+++ casa_manila_api/config/settings.py
@@ -129,6 +129,9 @@ USE_I18N = True
 USE_TZ = True
 
 
+# Default auto field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
