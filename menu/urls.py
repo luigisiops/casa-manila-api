79a0e714d54ff5e5eaeb195bac2b1b07073a277b
reiden-webber <reidenw@gmail.com>
2025-12-19 18:59:43 -0800

    Add urls for Order and ItemOrder CRUD
    This commit simply appends the proper urls to the router that come with
    the ViewSets.

--- menu/urls.py
+++ menu/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import FoodItemViewSet
+from .views import FoodItemViewSet, ItemOrderViewSet, OrderViewSet
 
 router = DefaultRouter()
 router.register(r'food-items', FoodItemViewSet)
+router.register(r'item-orders', ItemOrderViewSet)
+router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
