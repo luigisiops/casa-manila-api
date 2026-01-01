3b0a1670f4dbec34cf8cbd047803eaa88aadb41c
reiden-webber <reidenw@gmail.com>
2026-01-01 13:05:10 -0800

    Modify Order creation to include ItemOrder
    This co

--- menu/serializers.py
+++ menu/serializers.py
@@ -13,16 +13,33 @@ class ItemOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = ItemOrder
         fields = ['id', 'order_id', 'food_item', 'item_id', 'quantity', 'is_active']
-        read_only_fields = ['id']
+        read_only_fields = ['id', 'order_id']
 
 class OrderSerializer(serializers.ModelSerializer):
-    # TODO: Allow nested writes for creating orders with items
     item_orders = ItemOrderSerializer(many=True, read_only=True)
+    # Accept simple item_id and quantity for creation
+    items = serializers.ListField(
+        child=serializers.DictField(), write_only=True, required=False
+    )
 
     class Meta:
         model = Order
         fields = [
-            'id', 'pickup_datetime', 'customer_name', 'email', 'phone_number', 'item_orders',
-            'total_cost', 'store_id', 'is_completed', 'is_active'
+            'id', 'pickup_datetime', 'customer_name', 'email', 'phone_number',
+            'total_cost', 'store_id', 'is_completed', 'is_active',
+            'item_orders', 'items'
         ]
         read_only_fields = ['id']
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items', [])
+        order = Order.objects.create(**validated_data)
+        
+        for item_data in items_data:
+            ItemOrder.objects.create(
+                order_id=order,
+                item_id_id=item_data['item_id'],
+                quantity=item_data['quantity']
+            )
+        
+        return order
